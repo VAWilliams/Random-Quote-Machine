f2e75ea985bce409cdb12f9cb3377e30f9afb409
Vicario Williams <vawilliams@github.com>
2020-07-30 19:47:27 +0200

    Updates

---
+++ precache-manifest.1caf8dfe01c2c544c064db20fd2b86ea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b844d5134ebeab25e48eb1e4b7c6bbd3",
+    "url": "/Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "90b2fdcfc2f981b8ef9d",
+    "url": "/Random-Quote-Machine/static/css/2.4e3a83b6.chunk.css"
+  },
+  {
+    "revision": "12f6a6b548a9356d58e2",
+    "url": "/Random-Quote-Machine/static/css/main.26345ca6.chunk.css"
+  },
+  {
+    "revision": "90b2fdcfc2f981b8ef9d",
+    "url": "/Random-Quote-Machine/static/js/2.aa5798e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Quote-Machine/static/js/2.aa5798e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f6a6b548a9356d58e2",
+    "url": "/Random-Quote-Machine/static/js/main.c16f8a5e.chunk.js"
+  },
+  {
+    "revision": "b9e23066f9d379bd9edf",
+    "url": "/Random-Quote-Machine/static/js/runtime-main.1b36b324.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Random-Quote-Machine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Random-Quote-Machine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Random-Quote-Machine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Random-Quote-Machine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Random-Quote-Machine/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
